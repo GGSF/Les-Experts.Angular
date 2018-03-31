af221dd8b3192774e37211c9d21cffe84cddcdd4
geromegrignon <gerome.grignon.lp2@gmail.com>
2018-03-31 19:50:19 +0200

    readme known issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,8 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
-import { MatSidenav } from '@angular/material';
-import { DetailPieceOfEvidenceComponent } from './modules/piece-of-evidence/detail-piece-of-evidence/detail-piece-of-evidence.component';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<app-navigation></app-navigation>'
 })
-export class AppComponent implements OnInit {
-  title = 'app';
-  @ViewChild('sidenav') private sidenav: MatSidenav;
-
-  ngOnInit() {
-    this.sidenav.open();
-  }
+export class AppComponent {
 }
